7e7d08d0592a334ab3c4a1995a81e5c007c49701
Anton Kuzmin <furthertwoant@gmail.com>
2023-02-18 13:54:18 +0400

    Changed project structure

---
+++ LoginApp/Models/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  LoginApp
+//
+//  Created by Anton Kuzmin on 16.02.2023.
+//
+
+import Foundation
+
+struct User {
+    let login: String
+    let password: String
+    let person: Person
+}
+
+
+struct Person {
+    let name: String
+}
