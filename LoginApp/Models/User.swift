9649b69a6d054049c4fec5eca784ef235679b425
Anton Kuzmin <furthertwoant@gmail.com>
2023-02-19 00:53:44 +0400

    Refactoring

--- LoginApp/Models/User.swift
+++ LoginApp/Models/User.swift
@@ -8,21 +8,12 @@
 import Foundation
 
 struct User {
-//    let login = "User"
-//    let password = "pass"
-//    let person: Person
-    
     let login: String
     let password: String
     let person: Person
 }
 
 struct Person {
-//    let name = "Anton"
-//    let surname = "Kuzmin"
-//    let company = "LANIT"
-//    let job = "QA Automation Engineer"
-    
     let name: String
     let surname: String
     let company: String
