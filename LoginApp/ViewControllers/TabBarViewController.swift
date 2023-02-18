294550fac208f1069d0577541e2a493bcf2897d8
Anton Kuzmin <furthertwoant@gmail.com>
2023-02-18 20:03:38 +0400

    Added tab bar settings

---
+++ LoginApp/ViewControllers/TabBarViewController.swift
@@ -0,0 +1,16 @@
+//
+//  TabBarViewController.swift
+//  LoginApp
+//
+//  Created by Anton Kuzmin on 18.02.2023.
+//
+
+import UIKit
+
+final class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tabBar.layer.borderWidth = 0.5
+    }
+}
